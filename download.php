6240435a4f0d1afd15263ff401835e6aabafaf8b
cybernet2w <cybernet2w@gmail.com>
2010-05-23 15:58:01 +0000

    // updates
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // Modified By cybernet2u

--- download.php
+++ download.php
@@ -72,9 +72,9 @@ if (!is_file($filepath) || !is_readable($filepath))
 $f = urldecode($_GET["f"]);
 
 // pid code begin
-$result = get_result("SELECT pid FROM {$TABLE_PREFIX}users WHERE id=".$CURUSER['uid'],true,$btit_settings['cache_duration']);
-$row = mysql_fetch_assoc($result);
+$result = get_result("SELECT pid FROM {$TABLE_PREFIX}users WHERE id=".$CURUSER['uid'], true, $btit_settings['cache_duration']);
 $pid = $row[0]["pid"];
+
 if (!$pid)
    {
    $pid = md5(uniqid(rand(), true));
@@ -83,7 +83,7 @@ if (!$pid)
       do_sqlquery("UPDATE xbt_users SET torrent_pass='".$pid."' WHERE uid='".$CURUSER['uid']."'");
 }
 
-$result = get_result("SELECT * FROM {$TABLE_PREFIX}files WHERE info_hash='".$infohash."'",true,$btit_settings['cache_duration']);
+$result = get_result("SELECT * FROM {$TABLE_PREFIX}files WHERE info_hash='".$infohash."'", true, $btit_settings['cache_duration']);
 $row = $result[0];
 
 
