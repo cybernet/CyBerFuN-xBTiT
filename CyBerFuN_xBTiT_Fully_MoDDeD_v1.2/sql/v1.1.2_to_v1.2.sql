eb3312b252bedc5f8e9153ba99c42703ec340fd5
cybernet2w <cybernet2w@gmail.com>
2010-05-23 20:54:47 +0000

    // hacks updates
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // Modified By cybernet2u

--- CyBerFuN_xBTiT_Fully_MoDDeD_v1.2/sql/v1.1.2_to_v1.2.sql
+++ CyBerFuN_xBTiT_Fully_MoDDeD_v1.2/sql/v1.1.2_to_v1.2.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `{$db_prefix}users` ADD INDEX ( `smf_fid` );
+UPDATE `{$db_prefix}users_level` SET `prefixcolor` = '<span style=''color:#000000''>' WHERE `{$db_prefix}users_level`.`id` =3;
 UPDATE `{$db_prefix}users_level` SET `prefixcolor` = '<span style=''color:#428D67''>' WHERE `{$db_prefix}users_level`.`id` =6;
 UPDATE `{$db_prefix}users_level` SET `prefixcolor` = '<span style=''color:#FF8000''>' WHERE `{$db_prefix}users_level`.`id` =7;
