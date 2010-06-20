0bce2ed568b14cc3d17cb0c471c29c1945feea7c
cybernet <cybernet2u@yahoo.com>
2010-06-20 20:37:47 +0000

    // Google Analytics code added
    // in the near future will be a hack to disable / enable and change id hack
    
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // http://xDnS.ro/
    // http://yDnS.ro/
    // Modified By cybernet2u

--- include/CyBerFuN_xBTiT_version.php
+++ include/CyBerFuN_xBTiT_version.php
@@ -1,3 +1,16 @@
 <?php
-$CyBerFuN_xBTiT_version = 'v1.2 ( rev 551 )';
+$CyBerFuN_xBTiT_version = 'v1.2 ( rev 554 )';
+$google_tracking_code = '<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push([\'_setAccount\', \'UA-1234-5\']);
+  _gaq.push([\'_trackPageview\']);
+
+  (function() {
+    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
+    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
+    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>';
 ?>
