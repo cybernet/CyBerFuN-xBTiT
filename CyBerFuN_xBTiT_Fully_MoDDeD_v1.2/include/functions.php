deb30d923aff0cc2ae7d4dc83d285db2221fdbc9
cybernet <cybernet2u@yahoo.com>
2010-06-23 15:59:28 +0000

    // optimize
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://tracker.cyberfun.ro/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // http://xDnS.ro/
    // http://yDnS.ro/
    // Modified By cybernet2u

--- CyBerFuN_xBTiT_Fully_MoDDeD_v1.2/include/functions.php
+++ CyBerFuN_xBTiT_Fully_MoDDeD_v1.2/include/functions.php
@@ -363,10 +363,9 @@ function print_designer() {
      include($STYLEPATH.'/style_copyright.php');
      $CyBerFuN_xBTiT_version = '' . $CyBerFuN_xBTiT_version . '';
      $design_copyright = '' . $design_copyright . '';
-     $google_analytics = '' . $google_tracking_code . '';
   } else
-     $CyBerFuN_xBTiT_version = 'v1.2 ( rev 554 )';
-     $design_copyright = "[&nbsp;&nbsp;<u>CyBerFuN xBTiT " . $CyBerFuN_xBTiT_version . " By cybernet</u>: <a href=\"http://xList.ro/\" target=\"_blank\">xList Tracker</a>&nbsp;]<br /> [&nbsp;&nbsp;<u>xbtit " . $tracker_version . " By <a href=\"http://www.btiteam.org/\" target=\"_blank\">BTiTeam.org</a></u>&nbsp;]<br />\n" . $google_analytics . "";
+     $CyBerFuN_xBTiT_version = 'v1.2 ( rev 559 )';
+     $design_copyright = "[&nbsp;&nbsp;<u>CyBerFuN xBTiT " . $CyBerFuN_xBTiT_version . " By cybernet</u>: <a href=\"http://xList.ro/\" target=\"_blank\">xList Tracker</a>&nbsp;]<br /> [&nbsp;&nbsp;<u>xbtit " . $tracker_version . " By <a href=\"http://www.btiteam.org/\" target=\"_blank\">BTiTeam.org</a></u>&nbsp;]<br />";
   return $design_copyright;
 }
 
