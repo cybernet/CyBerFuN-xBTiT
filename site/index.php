d907c804b449d18d4bf3b202d9454fd1a93c2818
cybernet <cybernet2u@yahoo.com>
2010-12-04 06:15:18 +0000

    // & updates
    // site is creating...
    
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://cyberfun-xbtit.co.cc/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // http://xDnS.ro/
    // Modified By cybernet2u

---
+++ site/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// CyBerFuN.ro & xList.ro
+
+// CyBerFuN .::. index
+// http://tracker.cyberfun.ro/
+// http://www.cyberfun.ro/
+// http://xList.ro/
+// http://xDnS.ro/
+// http://yDnS.ro/
+// Modified By cybernet2u
+
+// CyBerFuN xBTiT Fully MoDDeD v1.2
+
+
+// https://cyberfun-xbtit.svn.sourceforge.net/svnroot/cyberfun-xbtit/trunk/site
+
+
+
+?>
