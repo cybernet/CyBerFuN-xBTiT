931450200eff1ca72d006d21235356611b1af949
cybernet <cybernet2u@yahoo.com>
2011-03-08 07:02:43 +0000

    // - Update the copyright to 2011. Released as xbtit 2.2.1 / http://bit.ly/h8WvOw
    // Have FuN
    
    // CyBerFuN.ro & xList.ro
    
    // CyBerFuN .::. xList //
    // http://cyberfun-xbtit.co.cc/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // http://xDnS.ro/
    // Modified By cybernet2u

--- CyBerFuN_xBTiT_Fully_MoDDeD_v2.0/votesview.php
+++ CyBerFuN_xBTiT_Fully_MoDDeD_v2.0/votesview.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////////////////////////////////////////
 // xbtit - Bittorrent tracker/frontend
 //
-// Copyright (C) 2004 - 2009  Btiteam
+// Copyright (C) 2004 - 2011  Btiteam
 //
 //    This file is part of xbtit.
 //
